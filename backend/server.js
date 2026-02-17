3664d63fa38ec427dc0d1c3dca5e2e5189241fc7
ILHAM NADHIR <noorulilham3@gmail.com>
2026-02-18 01:18:46 +0530

    Revert "mongo deployment"
    This reverts commit 20ff513b6930340dfce92e1cfe94cd6abf6510bc.

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
