932d117ff05cbc831b418b4c28a5163727a5e35e
ILHAM NADHIR <noorulilham3@gmail.com>
2026-03-24 16:48:06 +0530

    correction hosting otp

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -43,11 +43,16 @@ router.post('/send-otp', async (req, res) => {
         await newOTP.save();
 
         let transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // Use SSL/TLS
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
+            tls: {
+                rejectUnauthorized: false
+            }
         });
 
         let info = await transporter.sendMail({
